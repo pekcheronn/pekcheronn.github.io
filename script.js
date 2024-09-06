deba3fb197cabbbf901f2ff310edbe27424c6fc3
pekcheronn <133960406+pekcheronn@users.noreply.github.com>
2024-09-06 16:30:41 +0800

    Removed unnecessary files

--- script.js
+++
@@ -1,9 +0,0 @@
-/* script.js */
-document.addEventListener('mousemove', function(event) {
-    const x = event.clientX / window.innerWidth;
-    const y = event.clientY / window.innerHeight;
-    
-    const gradientBackground = document.querySelector('.gradient-background');
-    
-    gradientBackground.style.backgroundPosition = `${x * 100}% ${y * 100}%`;
-});
